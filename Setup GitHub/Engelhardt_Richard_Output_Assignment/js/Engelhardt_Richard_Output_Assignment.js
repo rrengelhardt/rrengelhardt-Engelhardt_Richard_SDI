e2b9926baeb1870cecabf5af90f7ae230756d04a
rrengelhardt <rrengelhardt@fullsail.edu>
2015-09-30 13:38:11 -0400

    Added variables
    Added variables with prompts.

--- Setup GitHub/Engelhardt_Richard_Output_Assignment/js/Engelhardt_Richard_Output_Assignment.js
+++ Setup GitHub/Engelhardt_Richard_Output_Assignment/js/Engelhardt_Richard_Output_Assignment.js
@@ -1,1 +1,17 @@
-alert("This is in the js file");
+/*
+    Richard Engelhardt
+    SDI 1510
+    Creating Variables and Output
+    09-30-2015
+ */
+
+// declare variables
+
+var streetName = prompt("Enter my address","");
+var city = prompt("Enter my city","");
+var state = prompt("Enter my state","");
+var miles = prompt("How many miles to work?","");
+var ownCar = prompt("Own a car true\false?","");
+var kilometers;
+var address;
+
