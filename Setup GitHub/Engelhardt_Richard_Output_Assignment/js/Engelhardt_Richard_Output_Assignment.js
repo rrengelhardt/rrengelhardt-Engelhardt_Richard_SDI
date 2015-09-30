7fca77dec54a33a5c758eff7fa60b043d7c7e9ca
rrengelhardt <rrengelhardt@fullsail.edu>
2015-09-30 16:05:27 -0400

    Final comments.
    Put in final comments.

--- Setup GitHub/Engelhardt_Richard_Output_Assignment/js/Engelhardt_Richard_Output_Assignment.js
+++ Setup GitHub/Engelhardt_Richard_Output_Assignment/js/Engelhardt_Richard_Output_Assignment.js
@@ -14,7 +14,6 @@ var myState;
 var myZip;
 var milesToWork;
 var myStringAge;
-// var myNumberAge;
 var numInput;
 var kilometers;
 var myAddress;
@@ -24,20 +23,35 @@ myStreetName = "Salmer Avenue";
 myCity = "Pontiac";
 myState = "MI";
 myZip = "48341";
-myAddress = myStreetNumber + " " + myStreetName + ", " + myCity + ", " + myState + " " + myZip;
 
+// concatenate address parts to make and display complete address
+
+myAddress = myStreetNumber + " " + myStreetName + ", " + myCity + ", " + myState + " " + myZip;
 document.write("My address is: " + myAddress);
 
 // input miles and calculate kilometers
 
 milesToWork = prompt("How many miles to drive to work?","");
-
 kilometers = parseFloat(milesToWork) * 1.60934;
 alert("That is " + kilometers + " kilometers");
 
+// get age
+
 myStringAge = prompt("How old do you think I am?","");
+
+/* check to see if age is a number, if not display false, otherwise display true
+    if the age is over 40 then display old, otherwise display young.
+*/
 
 if ((isNaN(myStringAge)) == true) {
     numInput = false;
     alert("Was a number input is " + numInput);
-}   else {}
+}   else {
+        numInput = true;
+        alert("Was a number input is " + numInput);
+        if (parseFloat(myStringAge) > 40) {
+            alert("You must think I am old");
+        } else {
+            alert("You must think I am young");
+        }
+}
