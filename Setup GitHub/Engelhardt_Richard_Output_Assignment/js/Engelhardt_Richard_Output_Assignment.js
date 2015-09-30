e57bc885d77bd5567dfb9d65054f78b9232141be
rrengelhardt <rrengelhardt@fullsail.edu>
2015-09-30 15:43:34 -0400

    Add if statement.
    Added if statement, and tested for number.

--- Setup GitHub/Engelhardt_Richard_Output_Assignment/js/Engelhardt_Richard_Output_Assignment.js
+++ Setup GitHub/Engelhardt_Richard_Output_Assignment/js/Engelhardt_Richard_Output_Assignment.js
@@ -13,19 +13,31 @@ var myCity;
 var myState;
 var myZip;
 var milesToWork;
-var ownCar;
+var myStringAge;
+// var myNumberAge;
+var numInput;
 var kilometers;
 var myAddress;
 
-myStreetNumber = "30"
+myStreetNumber = "30";
 myStreetName = "Salmer Avenue";
 myCity = "Pontiac";
 myState = "MI";
 myZip = "48341";
 myAddress = myStreetNumber + " " + myStreetName + ", " + myCity + ", " + myState + " " + myZip;
 
-document.write(myAddress);
+document.write("My address is: " + myAddress);
+
+// input miles and calculate kilometers
 
 milesToWork = prompt("How many miles to drive to work?","");
+
 kilometers = parseFloat(milesToWork) * 1.60934;
 alert("That is " + kilometers + " kilometers");
+
+myStringAge = prompt("How old do you think I am?","");
+
+if ((isNaN(myStringAge)) == true) {
+    numInput = false;
+    alert("Was a number input is " + numInput);
+}   else {}
