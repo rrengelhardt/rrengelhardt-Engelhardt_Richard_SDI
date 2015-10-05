09662b3f30b4da15f59bc4696bab3489b1136810
rrengelhardt <rrengelhardt@fullsail.edu>
2015-10-05 13:28:52 -0400

    After add width and height
    After adding the inputtting of the width and height.

--- Setup GitHub/Engelhardt_Richard_Expressions_Assignment/js/Engelhardt_Richard_Expressions_Assignment.js
+++ Setup GitHub/Engelhardt_Richard_Expressions_Assignment/js/Engelhardt_Richard_Expressions_Assignment.js
@@ -17,24 +17,28 @@ var numInput;   // variable to hold the result of if an input was Not a Number.
 var sideArray = new Array();
 
 index = 0;  // assign 0 to the array index
-console.log("After index = " +index);
+console.log("Line 20 After index = " +index);
 
 // console.log("Side is " +sideArray[index]);
 
 numInput = false;
 
 while (numInput == false) {
-sideArray[index] = prompt("Please enter side one, the length:  ", "");    // Enter the value for the measurement of side one
+//    sideArray[index] = prompt("Please enter side 1 the length ", 100);
+sideArray[index] = prompt("Line 28 enter the length ", "");
+console.log("Line 29 numInput = " +numInput);
+//    console.log("Line 29 sideArrary = " + sideArray[index]);
 
 // check to see if sidearray is a number, if not display false, otherwise display true
 
-console.log("numInput = " +numInput);
-    if ((isNaN(sideArray[index])) == true) {         // check to see if myStringFriends is Not a Number
+console.log("Line 32 numInput = " +numInput);
+//    if ((isNaN(sideArray[index])) == true) {         // check to see if myStringFriends is Not a Number
+    if ((isNaN(sideArray[index])) == true) {
         numInput = false;                           // if so set numInput to false
-        console.log("The amount entered was not a number.  Try again?");
+        console.log("Line 35 The amount entered was not a number.  Try again?");
     } else {                                      // else if myStringFriends is input as a number
         numInput = true;                        // set numInput to true
-        console.log("Was a number input is " + numInput);             // display numInput
+        console.log("Line 38 Was a number input is " + numInput);             // display numInput
     }
 
 }
@@ -46,36 +50,31 @@ while (numInput == false) {
 
 // check to see if sideArray is a number, if not display false, otherwise display true
 
-    console.log("numInput = " +numInput);
+    console.log("Line 50 numInput = " +numInput);
     if ((isNaN(sideArray[index])) == true) {         // check to see if myStringFriends is Not a Number
         numInput = false;                           // if so set numInput to false
-        console.log("The amount entered was not a number.  Try again?");
+        console.log("Line 53 The amount entered was not a number.  Try again?");
     } else {                                      // else if myStringFriends is input as a number
         numInput = true;                        // set numInput to true
-        console.log("Was a number input is " + numInput);             // display numInput
+        console.log("Line 56  Was a number input is " + numInput);             // display numInput
     }
 
 }
 
+numInput = false;
+index++;
+while (numInput == false) {
+    sideArray[index] = prompt("Please enter side three, the height:  ", "");    // Enter the value for the measurement of side one
 
+// check to see if sideArray is a number, if not display false, otherwise display true
 
+    console.log("Line 71 numInput = " +numInput);
+    if ((isNaN(sideArray[index])) == true) {         // check to see if myStringFriends is Not a Number
+        numInput = false;                           // if so set numInput to false
+        console.log("Line 74 The amount entered was not a number.  Try again?");
+    } else {                                      // else if myStringFriends is input as a number
+        numInput = true;                        // set numInput to true
+        console.log("Line 77  Was a number input is " + numInput);             // display numInput
+    }
 
-
-
-
-
-/*
-// concatenate address parts to make and display complete address
-
-myAddress = myStreetNumber + " " + myStreetName + ", " + myCity + ", " + myState + " " + myZip;
-document.write("My address is: " + myAddress);
-
-// input miles and calculate kilometers
-
-milesToWork = prompt("How many miles to drive to work?","");    // input miles
-kilometers = parseFloat(milesToWork) * 1.60934; // calculate kilometers by converting to a number
-alert("That is " + kilometers + " kilometers"); // display the number of kilometers
-
-myStringFriends = prompt("How many friends do you think I have?","");  // get number of friends
-
-*/
+}
