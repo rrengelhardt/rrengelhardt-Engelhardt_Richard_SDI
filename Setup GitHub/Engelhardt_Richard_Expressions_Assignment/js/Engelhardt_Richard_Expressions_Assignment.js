08789fc1510d530a9e976183fa585bb652a06523
rrengelhardt <rrengelhardt@fullsail.edu>
2015-10-06 18:18:42 -0400

    After final comments
    After final comments and testing.

--- Setup GitHub/Engelhardt_Richard_Expressions_Assignment/js/Engelhardt_Richard_Expressions_Assignment.js
+++ Setup GitHub/Engelhardt_Richard_Expressions_Assignment/js/Engelhardt_Richard_Expressions_Assignment.js
@@ -1,92 +1,80 @@
-/*
-    Richard Engelhardt
-    SDI 1510
-    Creating Variables and Output
-    10-06-2015
-
-    This program will accept the length, width, and height of a right triangular prism
-    and output the surface area of the prism.
+/** Richard Engelhardt
+ *  10-06-2015
+ *  Expressions Assignment
  */
 
+//  This program accepts as input the length, width, and height of a right triangular prism and outputs the surface area
 // declare variables
 
-var index;          // integer used to hold the value of the array index.
-var surfaceArea;    // holds the value of the surface area of the right triangular prism.
-var numInput;       // variable to hold the result of if an input was Not a Number.
-var sideArray = new Array();
-var widthSquared;
-var heightSquared;
-var hypotenuseSquared;
-var hypotenuse;
+var index;                      // integer used to hold the value of the array index
+var surfaceArea;                // holds the value of the surface area of the right triangular prism
+var numInput;                   // variable to hold the result of if an input was Not a Number
+var sideArray = new Array();    // array to hold values for sides of a triangular prism
+var widthSquared;               // holds the value of the width * width
+var heightSquared;              // holds the value of the height * height
+var hypotenuseSquared;          // holds the value of the hypotenuse * hypotenuse
+var hypotenuse;                 // holds the value of the hypotenuse
 
-index = 0;  // assign 0 to the array index
-console.log("Line 20 After index = " +index);
+index = 0;          // assign 0 to the array index
+numInput = false;   // tells whether a number was input
 
-// console.log("Side is " +sideArray[index]);
-
-numInput = false;
-
-while (numInput == false) {
-
-// input, assign values and check if Not a Number
-    sideArray[index] = prompt("Please enter side 1 the length ", 0);
-console.log("Line 29 numInput = " +numInput);
-//    console.log("Line 29 sideArrary = " + sideArray[index]);
-
-// check to see if sidearray is a number, if not display false, otherwise display true
-
-console.log("Line 32 numInput = " +numInput);
-//    if ((isNaN(sideArray[index])) == true) {         // check to see if myStringFriends is Not a Number
-    if ((isNaN(sideArray[index])) == true) {
-        numInput = false;                           // if so set numInput to false
-        console.log("Line 35 The amount entered was not a number.  Try again?");
-    } else {                                      // else if myStringFriends is input as a number
+while (numInput == false) {  // do while a number has not been input
+    // input, assign values and check if Not a Number
+    sideArray[index] = prompt("Please enter side 1 the length ", 0);    // get the value for side 1
+    if ((isNaN(sideArray[index])) == true) {    // check to see if sideArray is a number, if not display false, otherwise display true
+        numInput = false;                       // if so set numInput to false
+        console.log("Line 35 The amount entered was not a number.  Try again?");    // tell the user a number was not input
+    } else {                                    // else if is Not a Number is false
+        sideArray[index] = Number(sideArray[index]);    // convert the answer to a number
         numInput = true;                        // set numInput to true
-        console.log("Line 38 Was a number input is " + numInput);             // display numInput
-    }
-}
+        console.log("Was a number input is " + numInput);   // display whether a number was input
+    }   // end else
+}   // end while
 
-numInput = false;
-index++;
-while (numInput == false) {
-    sideArray[index] = prompt("Please enter side two, the width:  ", "");    // Enter the value for the measurement of side one
+numInput = false;               // reset whether a number was input
+index++;                        // increment the value used as the index for sideArray
 
-// check to see if sideArray is a number, if not display false, otherwise display true
+while (numInput == false) {     // so while a number has not been input
+    sideArray[index] = prompt("Please enter side two, the width:  ", "");    // Enter the value for the measurement of side two
 
-    console.log("Line 50 numInput = " +numInput);
-    if ((isNaN(sideArray[index])) == true) {         // check to see if myStringFriends is Not a Number
+    // check to see if sideArray is a number, if not display false, otherwise display true
+    if ((isNaN(sideArray[index])) == true) {         // check to see if sideArray is Not a Number
         numInput = false;                           // if so set numInput to false
-        console.log("Line 53 The amount entered was not a number.  Try again?");
-    } else {                                      // else if myStringFriends is input as a number
-        numInput = true;                        // set numInput to true
-        console.log("Line 56  Was a number input is " + numInput);             // display numInput
-    }
-}
+        console.log("The amount entered was not a number.  Try again?");  // tell the user the amount entered was not a number
+    } else {                                       // else if sideArray is input as a number
+        sideArray[index] = Number(sideArray[index]);    // convert the answer to a number
+        numInput = true;                          // set numInput to true
+    }   // end else
+}   // end while
 
-numInput = false;
-index++;
-while (numInput == false) {
-    sideArray[index] = prompt("Please enter side three, the height:  ", "");    // Enter the value for the measurement of side one
+numInput = false;               // reset whether a number was input
+index++;                        // increment the value used as the index for sideArray
 
-// check to see if sideArray is a number, if not display false, otherwise display true
+while (numInput == false) {     // so while a number has not been input
+    sideArray[index] = prompt("Please enter side three, the height:  ", "");    // Enter the value for the measurement of side three
 
-    console.log("Line 71 numInput = " +numInput);
-    if ((isNaN(sideArray[index])) == true) {         // check to see if myStringFriends is Not a Number
+    // check to see if sideArray is a number, if not display false, otherwise display true
+    if ((isNaN(sideArray[index])) == true) {         // check to see if sideArray is Not a Number
         numInput = false;                           // if so set numInput to false
-        console.log("Line 74 The amount entered was not a number.  Try again?");
-    } else {                                      // else if myStringFriends is input as a number
+        console.log("The amount entered was not a number.  Try again?");    // display a number was not entered
+    } else {                                      // else if sideArray is input as a number
+        sideArray[index] = Number(sideArray[index]);    // convert the answer to a number
         numInput = true;                        // set numInput to true
-        console.log("Line 77  Was a number input is " + numInput);             // display numInput
-    }
-}
+        console.log("Was a number input is " + numInput);             // display numInput
+    }   // end else
+}   //end while
 
-widthSquared = sideArray[index] * sideArray[index];
-index--;
-heightSquared = sideArray[index] * sideArray[index];
-hypotenuseSquared = widthSquared + heightSquared;
-hypotenuse = Math.sqrt(hypotenuseSquared);
+/* using the pythagrean theorem compute the hypotenuse then the surface area */
+widthSquared = sideArray[index] * sideArray[index];     // compute a squared, or in other words the width squared
+index--;                                                // decrease the value of the index by 1
+heightSquared = sideArray[index] * sideArray[index];    // compute b squared, or in other words the height squared
+hypotenuseSquared = widthSquared + heightSquared;       // compute c squared, or in other words the hypotenuse squared
+hypotenuse = Math.sqrt(hypotenuseSquared);              // compute the hypotenuse
 
-console.log("Line 85 Hypotenuse = " + hypotenuse);
-
+console.log("The Hypotenuse = " + hypotenuse);          // outpuy the hypotenuse
+/*********** calculate the surface area ****************/
 surfaceArea = (sideArray[1] * sideArray[2]) + (sideArray[0] * sideArray[1]) + (sideArray[0] * sideArray[2]) + (sideArray[0] * hypotenuse);
-console.log("The total surface area is :  " + surfaceArea);
+
+console.log("The total surface area of the right triangular prism is :  " + surfaceArea); // ouput the surface area
+
+/* I typed 9 for the length, 8 for the width, and 6 for the height and my calculator gave me 264 for the surface area */
