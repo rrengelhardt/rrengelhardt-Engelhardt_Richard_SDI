9c7eac5cbb14fc019a200f9747ac77cb0e0e470a
rrengelhardt <rrengelhardt@fullsail.edu>
2015-10-21 07:25:36 -0400

    After coding main
    after coding main

--- Setup GitHub/Engelhardt_Richard_Funcions Assisnment/js/Engelhardt_Richard_Functions_Assignment.js
+++ Setup GitHub/Engelhardt_Richard_Funcions Assisnment/js/Engelhardt_Richard_Functions_Assignment.js
@@ -7,52 +7,103 @@
 //  THis program finds Powervall Numbers, or FLorida Lottery numbers depending on the users choice
 // declare globalvariables
 
+alert("Line 10 inside script");
+
+var calcLotto = true;
+var numberCeil;
 var getLotto = true;               // boolean used to get a set of numbers or exit
-var Answer = "";
+var answer = "";
 var whatToGet= "";
 var lottoArray = new Array();
 
+console.log("Line 17 before function");
+
 function getNumbers(numberCeil) {
-    var randomNum = Math.random()
+    var randomNum = Math.random();
     var lottoArray2 = new Array();
-    var i;
+    var i = 0;
     if (numberCeil === 59) {
-    lottoArray2[i] = 59;
+        lottoArray2[i] = 59;
+        console.log("Line 25 inside function i = " + i);
+    }
 }
-for (i=1; 1<6; i++) {}
-
-
-
-
-
+    console.log("Line 26 after function");
+// for (i=1; i<6; i++) {console.log("Line22 i = " + i);}
 
 while (getLotto === true) {
-    Answer = prompt("Do you wish to play the lottery? Y/N");
-    If (Answer !== "y") || (Answer !== "N")
+    answer = prompt("Do you wish to play the lottery? Y/N");
+
+    switch (answer) { // switch to the correct case
+        case "":        // case empty string
+            console.log("Line 37 Please enter a value");  // display error message
+            break;      // exit the switch statement
+        case "N":       // if calcWhat is interest
+        case "n":       // if calcWhat is Interest
+            getLotto = false;
+            break;
+        case "Y":       // if calcWhat is principal
+        case "y":       // if calcWhat is Principal
+            calcLotto = true;
+            while (calcLotto === true) {
+                whatToGet = prompt("Line 46 Enter F for Florida, or P for Powerball");
+                if ((whatToGet === "F") || (whatToGet === "f")){
+                    console.log("Line 50 whattoo get = " + whatToGet);
+                    calcLotto = false;
+                    myArray = getNumbers(53);
+                }
+                else if ((whatToGet === "P") || (whatToGet === "p"))
+                {
+                    console.log("Line 54 whattoo get = " + whatToGet);
+                    calcLotto = false;
+                    myArray = getNumbers(59);
+                }
+                console.log("Line 57 calcLotto = " + calcLotto);
+            }
+            console.log("Line 59 calclotto - " + calcLotto);
+                break;
+        default :       // for everything else
+            console.log("Line 59 please enter F or P ");
+               break;// display error message
+    }   // end switch
+}
+
+
+
+
+/*
+    if((answer != "y") || (answer != "n"))
     {
         console.log("that was an invalid answer, try again");
     }
-
-
-
-/**
-else if (Answer == "N") {
-        getLotto === False;
+    if (answer === "n") {
+        getLotto = false;
+        console.log("Line 40 inside if getLotto = " + getLotto);
     }
-        else {
-        while ((whatToGet != "F") && (WhatToGet != "P"))
+    if (answer === "y") {
+
+        while ((whatToGet != "F") && (whatToGet != "P"))
         {
-        WhatToGet = Prompt{"Input F for Florida Lottery,or P for Powerball");}
-    }
-    if WhatToGet === "F" {
-        myArray = getNumbers(53);
-    }
-    else myArray = getNumbers(59);
+            whatToGet = prompt("Input F for Florida Lottery,or P for Powerball");
+        }
 
     }
- */
+
 }
 
+*/
+
+
+/*
+ else if (Answer === "N") {
+ getLotto === False;
+ console.log(("Line 43 getLotto = " + getLotto));
+ }
+
+        else {
 
 
 
+
+
+
+*/
